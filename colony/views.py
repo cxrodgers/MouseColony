accdf925ad30d98f9a665b955e8c7c3f60dfb1e4
Chris Rodgers <qqrodgers@gmail.com>
2016-07-03 13:08:52 -0400

    adding [user] to mouse's name in census view

--- colony/views.py
+++ colony/views.py
@@ -6,7 +6,10 @@ from .models import  Mouse, Cage
 # Create your views here.
 
 class IndexView(generic.ListView):
-    """Returns all cages sorted by name for the CensusView to display"""
+    """Returns all cages sorted by name for the CensusView to display
+    
+    See the template in templates/colony/index.html
+    """
     template_name = 'colony/index.html'
     model = Cage
     def get_queryset(self):
